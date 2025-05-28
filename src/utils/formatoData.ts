43f71d1c237dd47cf7a9fc579292d8196b2897e6
Arthur <arthur.berdusco1@gmail.com>
2025-05-28 00:40:01 -0300

    atualizado

--- src/utils/formatoData.ts
+++ src/utils/formatoData.ts
@@ -1,8 +1,6 @@
 export const formatDateForDisplay = (dateString: string) => {
     if (!dateString) return "";
     const date = new Date(dateString);
-
-    // Obter dia, mês e ano e adicionar zeros à esquerda quando necessário
     const day = String(date.getDate()).padStart(2, '0');
     const month = String(date.getMonth() + 1).padStart(2, '0'); // +1 porque mês começa do zero
     const year = date.getFullYear();
